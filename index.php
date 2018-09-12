c9398e1a92cdecc5f433011acc402c4dde5f0132
YonaSmilevitch <EstherJudith92>
2018-09-12 12:59:33 +0200

    Merge branch 'master' of APPOE
    # Optimizing

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if (!bot_detected()) {
     mehoubarim_updateVisitor($_SESSION['currentPageName']);
 }
 
-$CmsContent = new App\Plugin\Cms\CmsContent($Cms->getId(), LANG);
+$CmsContent = new \App\Plugin\Cms\CmsContent($Cms->getId(), LANG);
 $allContentArr = $CmsContent->getData();
 
 $pageContent = getContainerErrorMsg('<div class="container"><h4>' . trans('Cette page n\'existe pas') . '</h4></div>');
