dfecf02ff252f5d343e0ae183ced4afc2bc16876
YonaSmilevitch <EstherJudith92>
2019-06-13 15:56:39 +0200

    Merge branch 'master' of APPOE
    # Optimizing system & plugins & creating CMS Multi langues

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 <?php
+
+use App\Plugin\Cms\CmsContent;
+use App\Template;
+
 include($_SERVER['DOCUMENT_ROOT'] . '/header_template.php');
 
 if (class_exists('App\Plugin\Cms\Cms')) {
 
     //Update visitor stats
     if (!bot_detected()) {
-        mehoubarim_updateVisitor($_SESSION['currentPageName']);
+        mehoubarim_updateVisitor(ucfirst(mb_strtolower($_SESSION['currentPageName'])));
     }
 
     //Get page content
-    $CmsContent = new \App\Plugin\Cms\CmsContent($Cms->getId(), LANG);
+    $CmsContent = new CmsContent($Cms->getId(), LANG);
 
     //Show page content in template
-    $Template = new \App\Template(WEB_PATH . $Cms->getSlug() . '.php', $CmsContent->getData(), true);
+    $Template = new Template(WEB_PATH . $Cms->getFilename() . '.php', $CmsContent->getData(), true);
     echo $Template->get();
 }
 
