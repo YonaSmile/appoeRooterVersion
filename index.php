85f4f211328300a1221281a7d52da0c0e01c1440
YonaSmile <yonasmilevitch@gmail.com>
2020-05-12 15:06:34 +0200

    Maintenance and improvement

--- index.php
+++ index.php
@@ -2,22 +2,32 @@
 
 use App\Plugin\Cms\CmsContent;
 use App\Template;
+use App\Cache;
 
-include($_SERVER['DOCUMENT_ROOT'] . '/header_template.php');
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/app/system/middleware_public.php' );
+includePluginsFiles();
 
-if (class_exists('App\Plugin\Cms\Cms')) {
+if ( class_exists( 'App\Plugin\Cms\Cms' ) ) {
 
 	//Update visitor stats
-	if (!bot_detected()) {
-		mehoubarim_updateVisitor(getPageData());
+	if ( ! bot_detected() ) {
+		mehoubarim_updateVisitor( getPageData() );
 	}
 
 	//Get page content
-	$CmsContent = new CmsContent($Cms->getId(), LANG);
+	$CmsContent = new CmsContent( getPageId(), LANG );
+	$Cache      = new Cache( getPageSlug() . '.php', CACHE_DURATION );
 
-	//Show page content in template
-	$Template = new Template(WEB_PATH . $Cms->getFilename() . '.php', $CmsContent->getData(), true);
-	echo $Template->get();
+	//Show page content in template with cache
+	if ( ! $Cache->start() ):
+
+		include( WEB_PATH . 'header.php' );
+
+		$Template = new Template( WEB_PATH . getPageFilename() . '.php', $CmsContent->getData(), true );
+		echo $Template->get();
+
+		include( WEB_PATH . 'footer.php' );
+
+	endif;
+	$Cache->end();
 }
-
-include($_SERVER['DOCUMENT_ROOT'] . '/footer_template.php');
