4610cf4ea19bfb26fc66dd9cf6e40b82ec48312e
YonaSmilevitch <EstherJudith92>
2020-06-16 12:58:44 +0200

    Merge branch 'master' of APPOE
    # Optimizing system & plugins

--- index.php
+++ index.php
@@ -1,32 +1,33 @@
 <?php
 
+use App\Cache;
 use App\Plugin\Cms\CmsContent;
 use App\Template;
-use App\Cache;
 
-require_once( $_SERVER['DOCUMENT_ROOT'] . '/app/system/middleware_public.php' );
+require_once($_SERVER['DOCUMENT_ROOT'] . '/app/system/middleware_public.php');
+includePluginsFiles();
 
-if ( class_exists( 'App\Plugin\Cms\Cms' ) ) {
+if (class_exists('App\Plugin\Cms\Cms')) {
 
-	//Update visitor stats
-	if ( ! bot_detected() ) {
-		mehoubarim_updateVisitor( getPageData() );
-	}
+    //Update visitor stats
+    if (!bot_detected()) {
+        mehoubarim_updateVisitor(getPageData());
+    }
 
-	//Get page content
-	$CmsContent = new CmsContent( getPageId(), ( ! empty( $_GET['access_lang'] ) ? $_GET['access_lang'] : LANG ) );
-	$Cache      = new Cache( getPageSlug() . '.php' );
+    //Get page content
+    $CmsContent = new CmsContent($Cms->getId(), LANG);
+    $Cache = new Cache(getPageSlug() . '.php');
 
-	//Show page content in template with cache (if is user)
-	if ( isset( $_GET['access_method'] ) || ! $Cache->start() ):
+    //Show page content in template with cache (if is user)
+    if (!$Cache->start()):
 
-		inc( WEB_PATH . 'header.php' );
+        inc(WEB_PATH . 'header.php');
 
-		$Template = new Template( WEB_PATH . getPageFilename() . '.php', $CmsContent->getData(), true );
-		echo $Template->get();
+        $Template = new Template(WEB_PATH . getPageFilename() . '.php', $CmsContent->getData(), true);
+        echo $Template->get();
 
-		inc( WEB_PATH . 'footer.php' );
+        inc(WEB_PATH . 'footer.php');
 
-	endif;
-	$Cache->end();
+    endif;
+    $Cache->end();
 }
