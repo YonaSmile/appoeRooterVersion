b4b3db3c6ed138ee2b2be9ebe4d2d045068930e5
YonaSmilevitch <EstherJudith92>
2021-09-06 14:04:11 +0200

    Merge branch 'master' of APPOE
    # Optimizing system & plugins

--- cron.php
+++ cron.php
@@ -2,6 +2,14 @@
 
 use App\Hook;
 
+//Get APPOE System
 require_once($_SERVER['DOCUMENT_ROOT'] . '/app/main.php');
+
+//Set the page filename for getting functions from declared plugins (ini.main)
+setPageFilename('cron');
+
+//Get all plugins functions
 includePluginsFiles(true);
+
+//Attach a Hook
 Hook::apply('cron');
